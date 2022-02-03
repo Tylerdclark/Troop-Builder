23c2390da6a40f1ce0599e7d745b658d15657748
Tyler Clark <tylerc.thedeveloper@gmail.com>
2022-02-03 12:31:14 +0100

    This works "on my machine"

--- src/troops.js
+++ src/troops.js
@@ -1,6 +1,12 @@
-exports.handler = async function (event, context) {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Hello World" }),
-    };
-  }
+import randomNumber from './util.js';
+
+export async function handler(event) {
+	const queries = event.queryStringParameters;
+	const count = queries.count ? parseInt(queries.count) : 1;
+	const total = queries.total ? parseInt(queries.total) : 100;
+	const nums = randomNumber(count, total);
+	return {
+		statusCode: 200,
+		body: JSON.stringify(nums),
+	};
+}
